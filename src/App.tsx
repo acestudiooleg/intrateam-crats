aff5e2512812d686d60d5690ecde407e921ce254
Oleh Sudavnyi <osudav@dp518.softservecom.com>
2019-12-20 18:55:45 +0200

    add code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import * as React from 'react';
+import React, { Suspense, lazy }from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Settings from './pages/Settings';
 import ProtectedRoute from './containers/ProtectedRoute';
-
 import { Routes } from './constants';
 import { ConnectedRouter } from 'connected-react-router';
-
 import { history } from './store';
+
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const Login = lazy(() => import('./pages/Login'));
+const Settings = lazy(() => import('./pages/Settings'));
 
 export default () => (
   <div className="App">
     <ConnectedRouter history={history}>
-      <Switch>
-        <ProtectedRoute path={Routes.Settings} component={Settings} />
-        <Route path={Routes.Dashboard} exact={true} component={Dashboard} />
-        <Route path={Routes.Login} exact={true} component={Login} />
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <ProtectedRoute path={Routes.Settings} component={Settings} />
+          <Route path={Routes.Dashboard} exact={true} component={Dashboard} />
+          <Route path={Routes.Login} exact={true} component={Login} />
+        </Switch>
+      </Suspense>
     </ConnectedRouter>
   </div>
 );
