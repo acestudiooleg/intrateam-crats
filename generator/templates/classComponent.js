e9d48fb78632d3a58af5795dbbd2ed13f0d08d4b
Oleg Sudavnyi <acestudiooleg@gmail.com>
2019-12-27 21:08:07 +0200

    temp generator

---
+++ generator/templates/classComponent.js
@@ -0,0 +1,41 @@
+module.exports = ({ name, Name }) => `import React, { Component } from 'react';
+
+interface IProps {
+  initialConter: number;
+}
+
+interface IState {
+  counter: number;
+}
+
+export default class ${Name} extends Component<IProps, IState> {
+  state = {
+    counter: 0,
+  };
+  constructor(props) {
+    super(props);
+    this.state.counter = props.initialState;
+    this.plus = this.plus.bind(this);
+    this.minus = this.minus.bind(this);
+  }
+  plus() {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+  minus() {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello component {this.state.counter} ${name}</h1>
+        <button onClick={this.plus}>+</button>
+        <button onClick={this.minus}>-</button>
+      </div>
+    );
+  }
+}
+`;
