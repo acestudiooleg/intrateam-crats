db27428dd6a19b09a09cb2d5a74d3ef48f445170
Oleh Sudavnyi <osudav@dp518.softservecom.com>
2020-01-03 19:11:38 +0200

    added actions test helpers and action test generator

--- generator/templates/actionSpec.js
+++ generator/templates/actionSpec.js
@@ -1,15 +1,22 @@
-module.exports = ({ name, Name, NA_ME }) => `import {
+module.exports = ({ name, Name, NA_ME, naMe }) => `import { actionSeq, act, hasFunc, hasType } from '../utils/actions';
+import ${naMe}, {
   FETCH_${NA_ME},
   FETCH_${NA_ME}_SUCCESS,
   FETCH_${NA_ME}_FAILURE,
   fetch,
   fetchSuccess,
   fetchFailure,
+  types,
 } from './${name}';
 
-describe('Actions ${Name} - test case', () => {
-  test('unit test', () => {
-    expect(2 + 2).toEqual(4);
-  });
-});
+describe(
+  'Actions ${Name} - test case',
+  actionSeq(${naMe}, types, [
+    act(FETCH_${NA_ME}, fetch),
+    act(FETCH_${NA_ME}_SUCCESS, fetchSuccess, true),
+    act(FETCH_${NA_ME}_FAILURE, fetchFailure, true),
+    hasType(FETCH_${NA_ME}, FETCH_${NA_ME}_SUCCESS, FETCH_${NA_ME}_FAILURE),
+    hasFunc(fetch, fetchSuccess, fetchFailure),
+  ]),
+);
 `;
